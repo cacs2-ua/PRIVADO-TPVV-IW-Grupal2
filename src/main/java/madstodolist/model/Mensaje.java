13c91f05654dd27f8f08f7c490e998d3ea766a01
cris-SX <cris-SX@me.com>
2024-12-08 15:27:35 +0100

    Mensaje Model and Repository added

---
+++ src/main/java/madstodolist/model/Mensaje.java
@@ -0,0 +1,50 @@
+package madstodolist.model;
+
+import org.w3c.dom.Text;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "mensajes")
+public class Mensaje implements Serializable  {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Date fecha;
+
+    @NotNull
+    String contenido;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Mensaje that = (Mensaje) o;
+
+        // Si ambos objetos tienen un ID no nulo, comparamos por ID
+        if (this.id != null && that.id != null) {
+            return Objects.equals(this.id, that.id);
+        }
+
+        // Si no se pueden comparar por ID, consideramos que son diferentes
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        // Generamos el hashCode basado únicamente en el ID
+        return Objects.hash(id);
+    }
+
+}
--- src/main/java/madstodolist/repository/UsuarioRepository.java
+++ src/main/java/madstodolist/repository/UsuarioRepository.java
@@ -2,11 +2,13 @@
 
 package madstodolist.repository;
 
+import madstodolist.model.Incidencia;
 import madstodolist.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findById(Long id);
     Optional<Usuario> findByEmail(String email);
 }
