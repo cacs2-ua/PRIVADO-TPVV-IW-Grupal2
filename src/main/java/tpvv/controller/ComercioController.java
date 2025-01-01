eb228d0fdacd5f22aaec33cf27127486ea99eb4a
Daniel Ripoll Sanchez <drs35@alu.ua.es>
2025-01-01 13:54:33 +0100

    Vista listado comercios - añadida vista al tecnico

--- src/main/java/tpvv/controller/ComercioController.java
+++ src/main/java/tpvv/controller/ComercioController.java
@@ -63,7 +63,7 @@ public class ComercioController {
 
     }
 
-    @PostMapping("/creacomercio")
+    @PostMapping("/api/admin/crearcomercio")
     public String registrarComercio(ComercioData comercio, PersonaContactoData personaContacto, Model model) {
         ComercioData nuevoComercio = comercioService.crearComercio(comercio);
         if (personaContacto.getNombreContacto() != null) {
@@ -74,35 +74,7 @@ public class ComercioController {
         return "redirect:/api/admin/crearcomercio";
     }
 
-    /*
-    @GetMapping("/api/admin/comercios")
-    public String listarComercio(Model model) {
-        List<ComercioData> comercios = comercioService.recuperarTodosLosComercios();
-        model.addAttribute("comercios", comercios);
-
-
-        return "listadoComercio";
-
-    }
-
-
-
-    @GetMapping("/api/admin/comercios")
-    public String getComercios(@RequestParam(defaultValue = "0") int page, Model model) {
-        List<ComercioData> comerciosData = comercioService.recuperarTodosLosComercios();
-        Page<ComercioData> comerciosPage = comercioService.recuperarComerciosPaginados(comerciosData, page, 8);
-        int totalPages = comerciosPage.getTotalPages();
-        List<PaisData> paises = paisService.findAll();
-
-        model.addAttribute("paises", paises);
-        model.addAttribute("comercios", comerciosPage.getContent());
-        model.addAttribute("totalPages", totalPages);
-        model.addAttribute("currentPage", page);
-        return "listadoComercio";
-    }
-    /
-     */
-    @GetMapping("/api/admin/comercios")
+    @GetMapping("/api/tecnico-or-admin/comercios")
     public String getComercios(
             @RequestParam(required = false) Long id,
             @RequestParam(required = false) String nombre,
@@ -112,7 +84,8 @@ public class ComercioController {
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaHasta,
             @RequestParam(defaultValue = "0") int page,
             Model model) {
-        
+
+        UsuarioData usuario = usuarioService.findById(getUsuarioLogeadoId());
         List<ComercioData> todosLosComercios = comercioService.recuperarTodosLosComercios();
         List<ComercioData> comerciosFiltrados = comercioService.filtrarComercios(todosLosComercios, id, nombre, cif, pais, fechaDesde, fechaHasta);
 
@@ -121,7 +94,7 @@ public class ComercioController {
 
         List<PaisData> paises = paisService.findAll();
 
-        // Añadir atributos al modelo
+        model.addAttribute("usuario", usuario);
         model.addAttribute("paises", paises);
         model.addAttribute("comercios", comerciosPage.getContent());
         model.addAttribute("totalPages", totalPages);
@@ -130,17 +103,11 @@ public class ComercioController {
         return "listadoComercio";
     }
 
-    @PostMapping("/api/admin/comercios/activar/{id}")
-    @ResponseBody
-    public String activarComercio(@PathVariable(value="id") Long idComercio, RedirectAttributes flash, HttpSession session) {
-        comercioService.modificarEstadoComercio(idComercio, true);
-        return "";
-    }
 
-    @PostMapping("/api/admin/comercios/estado/{id}")
+    @PostMapping("/api/tecnico-or-admin/comercios/estado/{id}")
     public String desactivarComercio(@PathVariable(value="id") Long idComercio, RedirectAttributes flash, HttpSession session) {
         ComercioData comercio = comercioService.recuperarComercio(idComercio);
         comercioService.modificarEstadoComercio(idComercio, !comercio.getActivo());
-        return "redirect:/api/admin/comercios";
+        return "redirect:/api/tecnico-or-admin/comercios";
     }
 }
