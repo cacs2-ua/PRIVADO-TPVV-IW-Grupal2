8313589d9cb67341a0301ed6fb48017854fff5d6
Daniel Ripoll Sanchez <drs35@alu.ua.es>
2025-01-02 13:40:45 +0100

    Vistas usuario - perfil tecnico v0

---
+++ src/main/java/tpvv/controller/UsuariosController.java
@@ -0,0 +1,41 @@
+package tpvv.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import tpvv.authentication.ManagerUserSession;
+import tpvv.dto.ComercioData;
+import tpvv.dto.PersonaContactoData;
+import tpvv.dto.UsuarioData;
+import tpvv.service.ComercioService;
+import tpvv.service.PaisService;
+import tpvv.service.UsuarioService;
+
+@Controller
+public class UsuariosController {
+
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private ManagerUserSession managerUserSession;
+
+    @Autowired
+    private PaisService paisService;
+    @Autowired
+    private ComercioService comercioService;
+
+    private Long getUsuarioLogeadoId() {
+        return managerUserSession.usuarioLogeado();
+    }
+
+    @GetMapping("/api/tecnico/mis-datos")
+    public String perfilTecnico(Model model) {
+        UsuarioData usuario = usuarioService.findById(getUsuarioLogeadoId());
+        model.addAttribute("usuario", usuario);
+
+        return "perfilTecnico";
+    }
+}
